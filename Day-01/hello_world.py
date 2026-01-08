8c4abdad9d15b87f779adb79005bf4ac8f08286e
Jehan_rm09 <jehanmusabji@gmail.com>
2026-01-08 21:39:17 +0530

    feat: create Day 1 python script
    Write my first python code. Also learned how to connect my local folder to GitHub.

--- Day-01/hello_world.py
+++ Day-01/hello_world.py
@@ -1,4 +1,4 @@
 #This is my first Python script for #100DaysOfCode
-print("Hello GitHub!")
+print("Hello GitHub! welcome to my 100-days-of-code challenge")
 
 print("I am 1% done with my challenge!")
