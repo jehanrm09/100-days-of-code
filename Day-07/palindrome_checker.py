eb4507257d899bc6e3fc86f8b1401c95545cd6f1
Jehan_rm09 <jehanmusabji@gmail.com>
2026-01-14 21:12:09 +0530

    feat: Day 7 - Manual Palindrome validation algorithm
    Developed a script to detect palindromes by cleaning strings with .isalnum() and manually reversing sequences using negative-step range loops.

--- Day-07/palindrome_checker.py
+++ Day-07/palindrome_checker.py
@@ -11,3 +11,13 @@ for i in text:
 
     continue
 
+for i in range( len(new_text)-1 , -1 , -1):
+    reverse_text=reverse_text+new_text[i]
+
+print("-- Result --")
+print(f"Original (Cleaned): {new_text}")
+print(f"Reversed: {reverse_text}")
+if new_text == reverse_text:
+    print("It is a Palindrome")
+else:
+    print("It is not a Palindrome")
