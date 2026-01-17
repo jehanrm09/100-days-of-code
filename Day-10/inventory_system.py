3d4650cea3cf9dfcd531665564b78c1cae43158b
Jehan_rm09 <jehanmusabji@gmail.com>
2026-01-17 21:56:10 +0530

    feat: Day 10 Milestone - Inventory system using Dictionaries
    Reached the 10-day milestone! Implemented a key-value store with Hash Map logic, providing O(1) lookup efficiency and user input handling.

---
+++ Day-10/inventory_system.py
@@ -0,0 +1,22 @@
+inventor={"apple": 50,
+    "banana": 30,
+    "orange": 80,
+    "milk": 25,
+    "bread": 35}
+
+print(" --- Welcome to the store ---")
+print("Available : ",end=" ")
+for i in inventor.keys():
+    print(i,end=" ")
+
+print()
+while(True):
+    item_name=input("Enter item name to check price: ").lower()
+    if item_name=="exit":
+        print("Closing System.")
+        break
+    elif item_name in inventor:
+        print(f"The price of {item_name} is {inventor[item_name]}")
+    else:
+        print(f"Error: '{item_name}' is not in our inventory.")
+        
