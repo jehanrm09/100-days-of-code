ba9ebf98ed518f031234621f28115ff5f72ad3c0
Jehan_rm09 <jehanmusabji@gmail.com>
2026-01-18 20:16:37 +0530

    feat: Day 11 - Dynamic frequency counter using dictionaries
    Implemented a word frequency algorithm. Practiced dynamic dictionary key initialization and iterating through key-value pairs using .items().

--- Day-11/word_counter.py
+++ Day-11/word_counter.py
@@ -1,4 +1,20 @@
 data=["apple", "banana", "apple", "orange", "apple", "banana", "grape"]
 fruits={}
-count=0
 
+for i in data:
+    if i in fruits:
+        fruits[i]+=1
+    else:
+        fruits[i]=1
+
+x=max(fruits.values())
+print(x)
+
+print(f"Processing Data:{data}")
+print("--- Final Counts ---")
+for i,j in fruits.items():
+    print(f"{i}: {j}")
+    if x == j :
+        max1 = i
+
+print("Most frequent item: " , max1)
