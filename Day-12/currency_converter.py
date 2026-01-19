69da976207e7de04ad48771af921224eec2ce8fb
Jehan_rm09 <jehanmusabji@gmail.com>
2026-01-20 04:13:56 +0530

    feat: Day 12 - Reusable functions and return statements
    Transitioned to modular programming by defining functions. Practiced using parameters and return values to create a reusable currency conversion tool.

--- Day-12/currency_converter.py
+++ Day-12/currency_converter.py
@@ -4,4 +4,19 @@ def converter(amount,rate):
 
 usd_to_eur_rate=0.86
 usd_to_inr_rate=90.90
-usd_to_eur_rate
+usd_to_gbp_rate=0.74
+
+# amount in USD dollar
+my_amount=100
+
+euros = converter(my_amount, usd_to_eur_rate)
+rupees=converter(my_amount, usd_to_inr_rate)
+pounds=converter(my_amount, usd_to_gbp_rate)
+
+print(f"--- Currency Report for ${my_amount} ---")
+print(f"In Euros: €{euros}")
+print(f"In India: ₹{rupees}")
+print(f"In British Pounds: £{pounds}")
+
+# testing with different amount
+print(f"Testing for small amount($5) in Euros: €{converter(5,usd_to_eur_rate)}")
