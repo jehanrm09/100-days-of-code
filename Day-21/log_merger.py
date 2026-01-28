cc91abcd38e1dd03bf09c1025d1ffb58e96880d0
jehanrm09 <jehanmusabji@gmail.com>
2026-01-28 19:57:09 +0530

    Created log_merger
    created two text file by manually(via Code)

---
+++ Day-21/log_merger.py
@@ -0,0 +1,10 @@
+with open("work_task.txt","w") as f1:
+    f1.write("Create new textfile\n")
+    f1.write("Add text in textfile\n")
+    f1.write("close the textfile")
+
+with open("personal.txt","w") as f2:
+    f2.write("I am 2nd Year Computer Engineering Student\n")
+    f2.write("I’m currently on a journey to explore the vast world\n")
+    f2.write(" I love meeting new people")
+
