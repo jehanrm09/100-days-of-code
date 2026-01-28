9ca202214171bfa5281cc01193062af987943de3
jehanrm09 <jehanmusabji@gmail.com>
2026-01-28 20:33:03 +0530

    feat: Day 21 - File merging and batch processing
    Developed a script to consolidate multiple text files into a single master archive. Implemented nested file handling and error logging for missing source files.

--- Day-21/log_merger.py
+++ Day-21/log_merger.py
@@ -3,8 +3,20 @@ with open("work_task.txt","w") as f1:
     f1.write("Add text in textfile\n")
     f1.write("close the textfile")
 
-with open("personal.txt","w") as f2:
+with open("personal_task.txt","w") as f2:
     f2.write("I am 2nd Year Computer Engineering Student\n")
     f2.write("I’m currently on a journey to explore the vast world\n")
-    f2.write(" I love meeting new people")
+    f2.write("I love meeting new people\n")
 
+files_to_merger=["work_task.txt","personal_task.txt"]
+
+with open("master_log.txt","w") as f:
+    try:
+        for i in files_to_merger:
+            f3=open(f"{i}")
+            f.write(f3.read())
+            
+    except FileNotFoundError:
+        print("File Not Found")
+
+ 
