ce67332c159b6f4e10f6b5c1d9fa083b256483b5
jehanrm09 <jehanmusabji@gmail.com>
2026-01-31 23:05:59 +0530

    feat: Day 25 - Complex Logic Gates & Access Control
    Implemented multi-criteria decision making using AND/OR operators. Built a simulated security gate with nested feedback logic.

--- Day-24/security_gate.py
+++ Day-24/security_gate.py
@@ -6,3 +6,16 @@ age=int(input("Enter Your age: "))
 user=input("Enter Your Username: ").lower()
 password=input("Enter Your Password: ")
 
+if stored_pass==password and(age>=18 or user==admin_user):
+    print("\n Welcome to secure area.")
+
+    if user==admin_user:
+        print("Logged as Admin")
+        
+else:
+    print("Access Denied")
+
+    if password!=stored_pass:
+        print("Incorrect Password")
+    else:
+        print("Age requirements not met")
