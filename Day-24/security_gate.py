33063c0f1298f9ddaf1b05a4f444c43ecf1b5bdd
jehanrm09 <jehanmusabji@gmail.com>
2026-01-31 19:33:25 +0530

    Create security_gate.py

---
+++ Day-24/security_gate.py
@@ -0,0 +1,8 @@
+print("--- Secure System Portal ---")
+stored_pass="Student@123"
+admin_user="admin_zeh"
+
+age=int(input("Enter Your age: "))
+user=input("Enter Your Username: ").lower()
+password=input("Enter Your Password: ")
+
